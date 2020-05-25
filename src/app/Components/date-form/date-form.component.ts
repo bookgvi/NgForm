65bb92ad06a0739d0efdbe43136af46f3763e983
book <bookgvi@gmail.com>
2020-05-25 10:30:35 +0300

    Simple form with range of dates

---
+++ src/app/Components/date-form/date-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-date-form',
+  templateUrl: './date-form.component.html',
+  styleUrls: ['./date-form.component.scss']
+})
+export class DateFormComponent implements OnInit {
+  public formModel: FormGroup = new FormGroup({
+    formGroupName: new FormGroup({
+      from: new FormControl(''),
+      to: new FormControl('')
+    })
+  })
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DateFormComponent } from './Components/date-form/date-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
